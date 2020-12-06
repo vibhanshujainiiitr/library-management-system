fc68d489030f7bae368a08cd28ae0286a7afba90
Vibhanshu Jain <cs19b1027@iiitr.ac.in>
2020-12-06 12:17:25 +0530

    conn class added

--- src/librarymanagementvibhanshu/conn.java
+++ src/librarymanagementvibhanshu/conn.java
@@ -9,6 +9,32 @@ package librarymanagementvibhanshu;
  *
  * @author the_looser
  */
+
+// This class is used for creating connection JDBC 
+
+import java.sql.*;
+
 public class conn {
+    Connection c;
+    Statement s;
     
+    public conn()
+    {
+        try
+        {
+        Class.forName("com.mysql.jdbc.Driver");
+        
+        // Creating connection 
+        c = DriverManager.getConnection("jdbc:mysql:///library","root","mahavir99@");
+        
+        // We can run the queries with the help of this statequeries
+        s = c.createStatement();
+        }
+        
+        
+        catch( Exception e)
+        {
+               System.out.println(e);
+        }    
+    }
 }
