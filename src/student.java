f90388562be95d4f322689cbf11a924d508904c7
Vibhanshu Jain <cs19b1027@iiitr.ac.in>
2020-12-11 22:12:42 +0530

    Student Part added

---
+++ src/student.java
@@ -0,0 +1,135 @@
+// This is the page of the application
+
+
+// Importing the packages
+import java.sql.*;
+import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class student extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	static student mainframe;
+	private JPanel contentPane;
+	
+	// Launching the application
+	public static void main ( String[] args )
+	{
+		EventQueue.invokeLater(new Runnable() {
+			public void run()
+			{
+				try
+				{
+					mainframe = new student();
+					mainframe.setVisible(true);
+				}
+				catch (Exception e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		} );
+	}
+	
+	// Now we need to create the frame
+	public student() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(400, 400, 600, 550);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel student = new JLabel("Student Panel");
+		student.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		student.setForeground(Color.GRAY);
+		
+		JButton addStudent = new JButton("Add Student");
+	    addStudent.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		addStudent.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			newStudent.main(new String[]{});
+			mainframe.dispose();
+			}
+		});
+		
+		JButton delStudent = new JButton("Issue Book");
+		delStudent.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		delStudent.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			deleteStudent.main(new String[]{});
+			mainframe.dispose();
+			}
+		});
+		
+		JButton editDetails = new JButton("Return Book");
+		editDetails.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		editDetails.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			editStudents.main(new String[]{});
+			mainframe.dispose();
+			}
+		});
+		
+		JButton schStudent = new JButton("Book Record");
+		schStudent.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		schStudent.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				searchStudent.main(new sString[]{});
+				mainframe.dispose();
+			}
+		});
+		JButton home = new JButton("Home");
+		home.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		home.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				home.setVisible(true);
+				mainframe.dispose();
+			}
+		});
+		
+		
+		
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap(150, Short.MAX_VALUE)
+					.addComponent(student, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE)
+					.addGap(123))
+				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+					.addGap(134)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(addStudent, GroupLayout.PREFERRED_SIZE, 300, GroupLayout.PREFERRED_SIZE)
+						.addComponent(delStudent, GroupLayout.PREFERRED_SIZE, 300, GroupLayout.PREFERRED_SIZE)
+						.addComponent(editDetails, GroupLayout.PREFERRED_SIZE, 300, GroupLayout.PREFERRED_SIZE)
+						.addComponent(schStudent, GroupLayout.PREFERRED_SIZE, 300, GroupLayout.PREFERRED_SIZE)
+	      				.addComponent(home, GroupLayout.PREFERRED_SIZE, 300, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(109, Short.MAX_VALUE)
+		 ));
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(student, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
+					.addGap(11)
+					.addComponent(addStudent, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(delStudent, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(editDetails, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(schStudent, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(home, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(21, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+	
+}
